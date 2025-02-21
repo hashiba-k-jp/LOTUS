f82ca9e90471554e5ff0f03e335a2e7f913ab9b3
HASHIBA Keishi <hashiba.k.jp@gmail.com>
2025-02-21 12:47:06 +0900

    cahnge the number of spaces used to indent 2->4

--- AS_class.py
+++ AS_class.py
@@ -4,136 +4,136 @@ import ipaddress
 from routing_table import Routing_table
 
 class IP_address_generator:
-  def __init__(self):
-    self.index = 1 # To generate unique address
+    def __init__(self):
+        self.index = 1 # To generate unique address
 
-  def get_unique_address(self):
-    address = "10." + str(self.index // 256) + "." + str(self.index % 256) + ".0/24"
-    self.index += 1
-    return address
+    def get_unique_address(self):
+        address = "10." + str(self.index // 256) + "." + str(self.index % 256) + ".0/24"
+        self.index += 1
+        return address
 
 class AS_class_list:
-  def __init__(self):
-    self.class_list = {}
-    self.ip_gen = IP_address_generator()
+    def __init__(self):
+        self.class_list = {}
+        self.ip_gen = IP_address_generator()
 
-  def add_AS(self, as_number):
-    if not as_number in self.class_list.keys():
-      self.class_list[as_number] = AS_class(as_number, self.ip_gen.get_unique_address())
-    else:
-      print("Error: AS " + str(as_number) + " is already registered.", file=sys.stderr)
+    def add_AS(self, as_number):
+        if not as_number in self.class_list.keys():
+            self.class_list[as_number] = AS_class(as_number, self.ip_gen.get_unique_address())
+        else:
+            print("Error: AS " + str(as_number) + " is already registered.", file=sys.stderr)
 
-  def show_AS_list(self, sort_flag, best_flag, address):
+    def show_AS_list(self, sort_flag, best_flag, address):
 
-    keys = list(self.class_list.keys())
-    if sort_flag == True:
-      keys.sort()
+        keys = list(self.class_list.keys())
+        if sort_flag == True:
+            keys.sort()
 
-    for k in keys:
-      self.class_list[k].show_info(only_best=best_flag, address=address)
+        for k in keys:
+            self.class_list[k].show_info(only_best=best_flag, address=address)
 
-  def get_AS(self, as_number):
-    return self.class_list[as_number]
+    def get_AS(self, as_number):
+        return self.class_list[as_number]
 
-  def get_AS_list(self):
-    return self.class_list
+    def get_AS_list(self):
+        return self.class_list
 
-  def import_AS_list(self, import_list):
+    def import_AS_list(self, import_list):
 
-    self.class_list = {}
-    for a in import_list:
-      self.class_list[a["AS"]] = AS_class(a["AS"], a["network_address"])
-      self.class_list[a["AS"]].policy = a["policy"]
-      self.class_list[a["AS"]].routing_table.change_policy(a["policy"])
-      self.class_list[a["AS"]].routing_table.table = a["routing_table"]
+        self.class_list = {}
+        for a in import_list:
+            self.class_list[a["AS"]] = AS_class(a["AS"], a["network_address"])
+            self.class_list[a["AS"]].policy = a["policy"]
+            self.class_list[a["AS"]].routing_table.change_policy(a["policy"])
+            self.class_list[a["AS"]].routing_table.table = a["routing_table"]
 
 class AS_class:
-  def __init__(self, asn, address):
-    self.as_number = asn
-    self.network_address = address
-    self.policy = ["LocPrf", "PathLength"]
-    self.routing_table = Routing_table(self.network_address, self.policy)
+    def __init__(self, asn, address):
+        self.as_number = asn
+        self.network_address = address
+        self.policy = ["LocPrf", "PathLength"]
+        self.routing_table = Routing_table(self.network_address, self.policy)
 
-  def show_info(self, only_best=False, address=None):
-    print("====================")
-    print(f"AS NUMBER: {self.as_number}")
-    print(f"network: {self.network_address}")
-    print(f"policy: {self.policy}")
+    def show_info(self, only_best=False, address=None):
+        print("====================")
+        print(f"AS NUMBER: {self.as_number}")
+        print(f"network: {self.network_address}")
+        print(f"policy: {self.policy}")
 
-    table = self.routing_table.get_table()
-    addr_list = []
-    if address == None:
-      for addr in table.keys():
-        addr_list.append(ipaddress.ip_network(addr))
-      addr_list.sort()
-    else:
-      addr_list.append(address)
-
-    print("routing table: (best path: > )")
-    for addr in addr_list:
-      print(str(addr) + ":")
-      try:
-        for r in table[str(addr)]:
-          path = r["path"]
-          come_from = r["come_from"]
-          LocPrf = r["LocPrf"]
-          try:
-            aspv = r["aspv"]
-            if r["best_path"] == True:
-              print(f"> path: {path}, LocPrf: {LocPrf}, come_from: {come_from}, aspv: {aspv}")
-            elif only_best == True:
-              continue
-            else:
-              print(f"  path: {path}, LocPrf: {LocPrf}, come_from: {come_from}, aspv: {aspv}")
-          except KeyError:
-            if r["best_path"] == True:
-              print(f"> path: {path}, LocPrf: {LocPrf}, come_from: {come_from}")
-            elif only_best == True:
-              continue
-            else:
-              print(f"  path: {path}, LocPrf: {LocPrf}, come_from: {come_from}")
-      except KeyError:
-        print("No-Path")
-    print("====================")
-
-  def set_public_aspa(self, public_aspa_list):
-    self.routing_table.set_public_aspa(public_aspa_list)
-
-  def update(self, update_message):
-    if self.as_number in update_message["path"].split("-"):
-      return
-
-    route_diff = self.routing_table.update(update_message)
-    if route_diff == None:
-      return
-    else:
-      route_diff["path"] = str(self.as_number) + "-" + route_diff["path"]
-      return route_diff
-
-  def change_ASPV(self, message):
-    if message["switch"] == "on":
-      self.policy = ["LocPrf", "PathLength"]
-      self.policy.insert(int(message["priority"]) - 1, "aspv")
-    elif message["switch"] == "off":
-      self.policy = ["LocPrf", "PathLength"]
-    self.routing_table.change_policy(self.policy)
-
-  def receive_init(self, init_message):
-    best_path_list = self.routing_table.get_best_path_list()
-    new_update_message_list = []
-    update_src = self.as_number
-    update_dst = init_message["src"]
-    if init_message["come_from"] == "customer":
-      for r in best_path_list:
-        if r["path"] == "i": # the network is the AS itself
-          new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src, "network": r["network"]})
+        table = self.routing_table.get_table()
+        addr_list = []
+        if address == None:
+            for addr in table.keys():
+                addr_list.append(ipaddress.ip_network(addr))
+            addr_list.sort()
         else:
-          new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src + "-" + r["path"], "network": r["network"]})
-    else:
-      for r in best_path_list:
-        if r["come_from"] == "customer":
-          if r["path"] == "i": # the network is the AS itself
-            new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src, "network": r["network"]})
-          else:
-            new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src + "-" + r["path"], "network": r["network"]})
-    return new_update_message_list
+            addr_list.append(address)
+
+        print("routing table: (best path: > )")
+        for addr in addr_list:
+            print(str(addr) + ":")
+            try:
+                for r in table[str(addr)]:
+                    path = r["path"]
+                    come_from = r["come_from"]
+                    LocPrf = r["LocPrf"]
+                    try:
+                        aspv = r["aspv"]
+                        if r["best_path"] == True:
+                            print(f"> path: {path}, LocPrf: {LocPrf}, come_from: {come_from}, aspv: {aspv}")
+                        elif only_best == True:
+                            continue
+                        else:
+                            print(f"  path: {path}, LocPrf: {LocPrf}, come_from: {come_from}, aspv: {aspv}")
+                    except KeyError:
+                        if r["best_path"] == True:
+                            print(f"> path: {path}, LocPrf: {LocPrf}, come_from: {come_from}")
+                        elif only_best == True:
+                            continue
+                        else:
+                            print(f"  path: {path}, LocPrf: {LocPrf}, come_from: {come_from}")
+            except KeyError:
+                print("No-Path")
+        print("====================")
+
+    def set_public_aspa(self, public_aspa_list):
+        self.routing_table.set_public_aspa(public_aspa_list)
+
+    def update(self, update_message):
+        if self.as_number in update_message["path"].split("-"):
+            return
+
+        route_diff = self.routing_table.update(update_message)
+        if route_diff == None:
+            return
+        else:
+            route_diff["path"] = str(self.as_number) + "-" + route_diff["path"]
+            return route_diff
+
+    def change_ASPV(self, message):
+        if message["switch"] == "on":
+            self.policy = ["LocPrf", "PathLength"]
+            self.policy.insert(int(message["priority"]) - 1, "aspv")
+        elif message["switch"] == "off":
+            self.policy = ["LocPrf", "PathLength"]
+        self.routing_table.change_policy(self.policy)
+
+    def receive_init(self, init_message):
+        best_path_list = self.routing_table.get_best_path_list()
+        new_update_message_list = []
+        update_src = self.as_number
+        update_dst = init_message["src"]
+        if init_message["come_from"] == "customer":
+            for r in best_path_list:
+                if r["path"] == "i": # the network is the AS itself
+                    new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src, "network": r["network"]})
+                else:
+                    new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src + "-" + r["path"], "network": r["network"]})
+        else:
+            for r in best_path_list:
+                if r["come_from"] == "customer":
+                    if r["path"] == "i": # the network is the AS itself
+                        new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src, "network": r["network"]})
+                    else:
+                        new_update_message_list.append({"src": update_src, "dst": update_dst, "path": update_src + "-" + r["path"], "network": r["network"]})
+        return new_update_message_list
--- routing_table.py
+++ routing_table.py
@@ -1,193 +1,193 @@
 import re
 
 class BestPathNotExist(Exception):
-  pass
+    pass
 
 class Routing_table:
-  def __init__(self, network, policy):
-    self.table = {}
-    self.table[network] = [{"path": "i", "come_from": "customer", "LocPrf": 1000, "best_path": True}]
-    self.policy = policy
-    self.aspa_list = {}
+    def __init__(self, network, policy):
+        self.table = {}
+        self.table[network] = [{"path": "i", "come_from": "customer", "LocPrf": 1000, "best_path": True}]
+        self.policy = policy
+        self.aspa_list = {}
 
-  def change_policy(self, policy):
-    self.policy = policy
+    def change_policy(self, policy):
+        self.policy = policy
 
-  def set_public_aspa(self, public_aspa_list):
-    self.aspa_list = public_aspa_list
+    def set_public_aspa(self, public_aspa_list):
+        self.aspa_list = public_aspa_list
 
-  # MEMO    : This function MUST return one of ["Unknown", "Valid", "Invalid"]
-  def verify_pair(self, customer_as, provider_as):
-    try:
-      candidate_provider_list = self.aspa_list[customer_as]
-    except KeyError:
-      return "Unknown"
+    # MEMO    : This function MUST return one of ["Unknown", "Valid", "Invalid"]
+    def verify_pair(self, customer_as, provider_as):
+        try:
+            candidate_provider_list = self.aspa_list[customer_as]
+        except KeyError:
+            return "Unknown"
 
-    if provider_as in candidate_provider_list:
-      return "Valid"
-    else:
-      return "Invalid"
-
-  def aspv(self, route, neighbor_as):
-
-    ###
-    ### Referencing Internet-Draft draft-ietf-sidrops-aspa-verification-08
-    ### https://www.ietf.org/archive/id/draft-ietf-sidrops-aspa-verification-08.txt
-    ###
-
-    p = route["path"]
-    path_list = p.split("-")
-
-    if route["come_from"] in ["customer", "peer"]:
-
-      if path_list[0] != neighbor_as:
-        return "Invalid"
-
-      try:
-        index = -1
-        semi_state = "Valid"
-        while True:
-          pair_check = self.verify_pair(path_list[index], path_list[index - 1])
-          if pair_check == "Invalid":
+        if provider_as in candidate_provider_list:
+            return "Valid"
+        else:
             return "Invalid"
-          elif pair_check == "Unknown":
-            semi_state = "Unknown"
-          index -= 1
-      except IndexError:  # the end of checking
-        pass
 
-      return semi_state
+    def aspv(self, route, neighbor_as):
 
-    elif route["come_from"] == "provider":
+        ###
+        ### Referencing Internet-Draft draft-ietf-sidrops-aspa-verification-08
+        ### https://www.ietf.org/archive/id/draft-ietf-sidrops-aspa-verification-08.txt
+        ###
 
-      if path_list[0] != neighbor_as:
-        return "Invalid"
+        p = route["path"]
+        path_list = p.split("-")
 
-      try:
-        index = -1
-        semi_state = "Valid"
-        upflow_fragment = True
-        while True:
-          if upflow_fragment == True:
-            pair_check = self.verify_pair(path_list[index], path_list[index - 1])
-            if pair_check == "Invalid":
-              upflow_fragment = False
-            elif pair_check == "Unknown":
-              semi_state = "Unknown"
-            index -= 1
-          elif upflow_fragment == False:
-            # I-D version: It is thought to be wrong.
-            # pair_check = self.verify_pair(path_list[index - 1], path_list[index])
-            pair_check = self.verify_pair(path_list[index], path_list[index + 1])
-            if pair_check == "Invalid":
-              return "Invalid"
-            elif pair_check == "Unknown":
-              semi_state = "Unknown"
-            index -= 1
-      except IndexError:  # the end of checking
-        pass
+        if route["come_from"] in ["customer", "peer"]:
 
-      return semi_state
+            if path_list[0] != neighbor_as:
+                return "Invalid"
 
-    else:
-      assert False, f"Invalid route sender : {route["""come_from"""]}"
+            try:
+                index = -1
+                semi_state = "Valid"
+                while True:
+                    pair_check = self.verify_pair(path_list[index], path_list[index - 1])
+                    if pair_check == "Invalid":
+                        return "Invalid"
+                    elif pair_check == "Unknown":
+                        semi_state = "Unknown"
+                    index -= 1
+            except IndexError:  # the end of checking
+                pass
 
-  def update(self, update_message):
-    network = update_message["network"]
-    path = update_message["path"]
-    come_from = update_message["come_from"]
+            return semi_state
 
-    # QUESTION: ここの値(50, 100, 200)はなんらかのRFCで定められている？もしくは慣習的なものか、このLOTUSの実装で適当に定められたものか？
-    if come_from == "peer":
-      locpref = 100
-    elif come_from == "provider":
-      locpref = 50
-    elif come_from == "customer":
-      locpref = 200
+        elif route["come_from"] == "provider":
 
-    new_route = {"path": path, "come_from": come_from, "LocPrf": locpref}
+            if path_list[0] != neighbor_as:
+                return "Invalid"
 
-    if "aspv" in self.policy:
-      new_route["aspv"] = self.aspv(new_route, update_message["src"])
+            try:
+                index = -1
+                semi_state = "Valid"
+                upflow_fragment = True
+                while True:
+                    if upflow_fragment == True:
+                        pair_check = self.verify_pair(path_list[index], path_list[index - 1])
+                        if pair_check == "Invalid":
+                            upflow_fragment = False
+                        elif pair_check == "Unknown":
+                            semi_state = "Unknown"
+                        index -= 1
+                    elif upflow_fragment == False:
+                        # I-D version: It is thought to be wrong.
+                        # pair_check = self.verify_pair(path_list[index - 1], path_list[index])
+                        pair_check = self.verify_pair(path_list[index], path_list[index + 1])
+                        if pair_check == "Invalid":
+                            return "Invalid"
+                        elif pair_check == "Unknown":
+                            semi_state = "Unknown"
+                        index -= 1
+            except IndexError:  # the end of checking
+                pass
 
-    try:
-      new_route["best_path"] = False
-      self.table[network].append(new_route)
+            return semi_state
 
-      # select best path
-      best = None
-      for r in self.table[network]:
-        if r["best_path"] == True:
-          best = r
-          break
-      if best == None:
-        raise BestPathNotExist
-
-      for p in self.policy:
-        if p == "LocPrf":
-          if new_route["LocPrf"] > best["LocPrf"]:
-            new_route["best_path"] = True
-            best["best_path"] = False
-            return {"path": new_route["path"], "come_from": new_route["come_from"], "network": network}
-          elif new_route["LocPrf"] == best["LocPrf"]:
-            continue
-          elif new_route["LocPrf"] < best["LocPrf"]:
-            return None
-        elif p == "PathLength":
-          new_length = len(new_route["path"].split("-"))
-          best_length = len(best["path"].split("-"))
-          if new_length < best_length:
-            new_route["best_path"] = True
-            best["best_path"] = False
-            return {"path": new_route["path"], "come_from": new_route["come_from"], "network": network}
-          elif new_length == best_length:
-            continue
-          elif new_length > best_length:
-            return None
-        elif p == "aspv":
-          if new_route["aspv"] == "Invalid":
-            return None
         else:
-            assert False, f"Invalid routing policy : {p}"
+            assert False, f"Invalid route sender : {route["""come_from"""]}"
 
-    # QUESTION: ここのexceptionはどこで入る？ new_route["aspv"] でKeyErrorになりうるけどその時には self.table[network] = [new_route] は何？
-    # MEMO    : 割と最初の self.table[network] でそもそもそのNWにrouteが登録されていない時っぽい。** new_route["aspv"] ではKeyErrorにはなり得ない。**
-    except KeyError:
-      if self.policy[0] == "aspv":
-        if new_route["aspv"] == "Invalid":
-          new_route["best_path"] = False
-          self.table[network] = [new_route]
-          return None
-        else:
-          new_route["best_path"] = True
-          self.table[network] = [new_route]
-          return {"path": path, "come_from": come_from, "network": network}
-      else:
-        new_route["best_path"] = True
-        self.table[network] = [new_route]
-        return {"path": path, "come_from": come_from, "network": network}
+    def update(self, update_message):
+        network = update_message["network"]
+        path = update_message["path"]
+        come_from = update_message["come_from"]
 
-    except BestPathNotExist:
-      if self.policy[0] == "aspv":
-        if new_route["aspv"] == "Invalid":
-          return None
-        else:
-          new_route["best_path"] = True
-          return {"path": path, "come_from": come_from, "network": network}
-      else:
-        new_route["best_path"] = True
-        return {"path": path, "come_from": come_from, "network": network}
+        # QUESTION: ここの値(50, 100, 200)はなんらかのRFCで定められている？もしくは慣習的なものか、このLOTUSの実装で適当に定められたものか？
+        if come_from == "peer":
+            locpref = 100
+        elif come_from == "provider":
+            locpref = 50
+        elif come_from == "customer":
+            locpref = 200
 
-  def get_best_path_list(self):
+        new_route = {"path": path, "come_from": come_from, "LocPrf": locpref}
 
-    best_path_list = []
+        if "aspv" in self.policy:
+            new_route["aspv"] = self.aspv(new_route, update_message["src"])
 
-    for network in self.table.keys():
-      for route in self.table[network]:
-        if route["best_path"] == True:
-          best_path_list.append(dict({"network": network}, **route))
+        try:
+            new_route["best_path"] = False
+            self.table[network].append(new_route)
 
-    return best_path_list
+            # select best path
+            best = None
+            for r in self.table[network]:
+                if r["best_path"] == True:
+                    best = r
+                    break
+            if best == None:
+                raise BestPathNotExist
 
-  def get_table(self):
-    return self.table
+            for p in self.policy:
+                if p == "LocPrf":
+                    if new_route["LocPrf"] > best["LocPrf"]:
+                        new_route["best_path"] = True
+                        best["best_path"] = False
+                        return {"path": new_route["path"], "come_from": new_route["come_from"], "network": network}
+                    elif new_route["LocPrf"] == best["LocPrf"]:
+                        continue
+                    elif new_route["LocPrf"] < best["LocPrf"]:
+                        return None
+                elif p == "PathLength":
+                    new_length = len(new_route["path"].split("-"))
+                    best_length = len(best["path"].split("-"))
+                    if new_length < best_length:
+                        new_route["best_path"] = True
+                        best["best_path"] = False
+                        return {"path": new_route["path"], "come_from": new_route["come_from"], "network": network}
+                    elif new_length == best_length:
+                        continue
+                    elif new_length > best_length:
+                        return None
+                elif p == "aspv":
+                    if new_route["aspv"] == "Invalid":
+                        return None
+                else:
+                        assert False, f"Invalid routing policy : {p}"
+
+        # QUESTION: ここのexceptionはどこで入る？ new_route["aspv"] でKeyErrorになりうるけどその時には self.table[network] = [new_route] は何？
+        # MEMO    : 割と最初の self.table[network] でそもそもそのNWにrouteが登録されていない時っぽい。** new_route["aspv"] ではKeyErrorにはなり得ない。**
+        except KeyError:
+            if self.policy[0] == "aspv":
+                if new_route["aspv"] == "Invalid":
+                    new_route["best_path"] = False
+                    self.table[network] = [new_route]
+                    return None
+                else:
+                    new_route["best_path"] = True
+                    self.table[network] = [new_route]
+                    return {"path": path, "come_from": come_from, "network": network}
+            else:
+                new_route["best_path"] = True
+                self.table[network] = [new_route]
+                return {"path": path, "come_from": come_from, "network": network}
+
+        except BestPathNotExist:
+            if self.policy[0] == "aspv":
+                if new_route["aspv"] == "Invalid":
+                    return None
+                else:
+                    new_route["best_path"] = True
+                    return {"path": path, "come_from": come_from, "network": network}
+            else:
+                new_route["best_path"] = True
+                return {"path": path, "come_from": come_from, "network": network}
+
+    def get_best_path_list(self):
+
+        best_path_list = []
+
+        for network in self.table.keys():
+            for route in self.table[network]:
+                if route["best_path"] == True:
+                    best_path_list.append(dict({"network": network}, **route))
+
+        return best_path_list
+
+    def get_table(self):
+        return self.table
